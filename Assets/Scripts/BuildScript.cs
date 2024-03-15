7cd2de6ead405432e733936baa9b690bd42fcb45
aaron <qkrsogusl3@gmail.com>
2024-03-15 12:09:29 +0900

    add default BulidScript

---
+++ Assets/Scripts/BuildScript.cs
@@ -0,0 +1,10 @@
+namespace UnityBuilderAction
+{
+    public static class BuildScript
+    {
+        public static void Build()
+        {
+            System.Console.WriteLine("run Build");
+        }
+    }
+}
